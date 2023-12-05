12234e439b89514f7d3809be67e14cb30a1f428a
AdahMilly <oamildred@gmail.com>
2023-12-05 16:36:21 +0300

    Add project files.

---
+++ PostsMiniProject/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostsMiniProject.Models
+{
+    public class User
+    {
+        //Step One: Display all users from the user API on the console 
+
+        //Step two:  on selecting a user display his posts on the console 
+
+        //Step three:  On selecting a post display the posts comments on the console
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
