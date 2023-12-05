267b83c8da6a4b45e55878c6a16f5d02df6c8908
AdahMilly <oamildred@gmail.com>
2023-12-05 16:51:24 +0300

    interfaces

---
+++ PostsMiniProject/Services/IService/IUser.cs
@@ -0,0 +1,18 @@
+﻿using PostsMiniProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostsMiniProject.Services.IService
+{
+    public interface IUser
+    {
+        Task<string> AddNewUser(AddUser newUser);
+
+        Task<List<User>> GetAllUser();
+
+        Task<User> GetUserById(int id);
+    }
+}
